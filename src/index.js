6942de144e5d1c69dddc1ffc15dd9ccb3780fc71
Yesset Bimanov <eset.bimanov@gmail.com>
2024-07-30 12:40:33 +0500

    feat: add toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 module.exports = function toReadable (number) {
-  
+  module.exports = function toReadable(number) {
+    const ones = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const teens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const hundreds = "hundred";
+
+    if (number < 10) {
+        return ones[number];
+    } else if (number < 20) {
+        return teens[number - 10];
+    } else if (number < 100) {
+        const ten = Math.floor(number / 10);
+        const remainder = number % 10;
+        return tens[ten] + (remainder ? " " + ones[remainder] : "");
+    } else if (number < 1000) {
+        const hundred = Math.floor(number / 100);
+        const remainder = number % 100;
+        return (
+            ones[hundred] +
+            " " +
+            hundreds +
+            (remainder ? " " + toReadable(remainder) : "")
+        );
+    }
+};
 }

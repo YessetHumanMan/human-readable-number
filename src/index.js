ac9615af0e6b33186d570a21d11603e1e977f859
YessetHumanMan <eset.bimanov@gmail.com>
2025-07-30 15:44:21 +0500

    feat: function toRedable run

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (number === 0) return 'zero';
+
+  let result = '';
+  let n = number;
+
+  if (n >= 100) {
+    result += `${ones[Math.floor(n / 100)]} hundred`;
+    n %= 100;
+    if (n > 0) result += ' ';
+  }
+
+  if (n < 20 && n > 0) {
+    result += ones[n];
+  } else if (n >= 20) {
+    result += tens[Math.floor(n / 10)];
+    if (n % 10 > 0) {
+      result += ` ${ones[n % 10]}`;
+    }
+  }
+
+  return result;
 };
